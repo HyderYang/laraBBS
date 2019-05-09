f8405c3f7279af27b3816448f31f75fefba5ad26
hyder <yanghuan0613@gmail.com>
2019-05-09 16:09:15 +0800

    用户创建护体

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -2,40 +2,29 @@
 
 namespace App\Http\Requests;
 
-class TopicRequest extends Request
-{
-    public function rules()
-    {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
-    }
-
-    public function messages()
-    {
-        return [
-            // Validation messages
-        ];
-    }
+class TopicRequest extends Request {
+	
+	public function rules() {
+		switch($this->method()) {
+			case 'POST':
+			case 'PUT':
+			case 'PATCH':
+				return [
+					'title' => 'required|min:2',
+					'body' => 'required|min:3',
+					'category_id' => 'required|numeric',
+				];
+			case 'GET':
+			case 'DELETE':
+			default:
+				return [];
+		}
+	}
+	
+	public function messages() {
+		return [
+			'title.min' => '标题至少两个字符',
+			'body.min' => '内容至少三个字符',
+		];
+	}
 }
--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -6,16 +6,13 @@ use App\Models\User;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
-
-class UserObserver
-{
-    public function creating(User $user)
-    {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
-    }
+class UserObserver {
+	
+	public function creating(User $user) {
+		//
+	}
+	
+	public function updating(User $user) {
+		//
+	}
 }
