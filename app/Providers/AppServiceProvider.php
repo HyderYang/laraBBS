657c631d8311646449f468da324bda32be9cdecc
hyder <yanghuan0613@gmail.com>
2019-05-13 16:57:07 +0800

    添加多用户切换

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider {
 	 * @return void
 	 */
 	public function register() {
-		//
+		if (app()->isLocal()) {
+			$this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+		}
 	}
 	
 	/**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,10 +31,14 @@
 		  @yield('content')
 
     </div>
-	
+    
 	  @include('layouts._footer')
   </div>
-	
+
+  @if (app()->isLocal())
+    @include('sudosu::user-selector')
+  @endif
+ 
 	<!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
   
