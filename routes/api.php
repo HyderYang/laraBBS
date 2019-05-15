c43a60dc5cb07fe65255d8f6762effc81ca47868
hyder <yanghuan0613@gmail.com>
2019-05-15 17:31:12 +0800

    接口请求频率

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,25 @@
 
 $api = app('Dingo\Api\Routing\Router');
 
+// v1 版本
 $api->version('v1', [
+	// 命名空间
 	'namespace' => 'App\Http\Controllers\Api'
 ], function($api){
-	// 短信验证码
-	$api->post('verificationCodes', 'VerificationCodesController@store')
-		->name('api.verificationCodes.store');
-	// 用户注册
-	$api->post('users', 'UsersController@store')
-		->name('api.users.store');
+	$api->group([
+		// 固定验证
+		'middleware' => 'api.throttle',
+		// 次数
+		'limit' => config('api.rate_limits.sign.limit'),
+		// 时常 单位分钟
+		'expires' => config('api.rate_limits.sign.expires'),
+	], function($api){
+		
+		// 短信验证码
+		$api->post('verificationCodes', 'VerificationCodesController@store')
+			->name('api.verificationCodes.store');
+		// 用户注册
+		$api->post('users', 'UsersController@store')
+			->name('api.users.store');
+	});
 });
